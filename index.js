4c84aebc1dc607b245573b68f41cc79826183701
underwritter <ntpp96@mail.ru>
2023-02-25 13:33:48 +0600

    adding functionality for swiping from the keyboard

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const CLIENT_ID = `vAI4Z9I9EpZGSfHRDc0LT6KFfpQDnFWJ5j_MveULFzM`;
 const slider = document.getElementById("slider");
 
 let state = [];
-let currenySlide;
+let currentlySlide;
+
+const leftKeyCode = 37;
+const rightKeyCode = 39;
 
 const fetchPhotos = async () => {
   try {
@@ -12,7 +15,7 @@ const fetchPhotos = async () => {
 
     if (responce.ok && data.length) {
       state = data;
-      currenySlide = data[0].id;
+      currentlySlide = data[0].id;
       setPhotos();
     }
   } catch (err) {
@@ -23,8 +26,8 @@ const fetchPhotos = async () => {
 const renderItem = () => {
   return state
     .map(({urls: {regular}, user: {name}, id}) => {
-      const isActive = currenySlide === id ? "active" : "";
-      return `<div class="slide ${isActive}" style="background-image: url(${regular})">
+      const isActive = currentlySlide === id ? "active" : "";
+      return `<div id="${id}" class="slide ${isActive}" style="background-image: url(${regular})">
                 <div class="slide-text">
                 <span>photo by</span>
                 ${name}
@@ -34,18 +37,49 @@ const renderItem = () => {
     .join("");
 };
 
+const getActiveSlide = () => document.querySelector(".active");
 const handleClick = ({currentTarget}) => {
-  const slides = document.querySelectorAll(".slide");
-  const {id} = currentTarget.dataset;
+  const {id} = currentTarget.id;
 
-  slides.forEach((slide) => slide.classList.remove("active"));
+  getActiveSlide().classList.remove("active");
   currentTarget.classList.add("active");
-  currenySlide = id;
+  currentlySlide = id;
 };
 
 const setPhotos = () => {
   slider.innerHTML = renderItem();
   const slides = document.querySelectorAll(".slide");
+
+  document.addEventListener("keydown", (e) => {
+    const currentNode = getActiveSlide();
+    const currentSlideIdx = state.findIndex((item) => {
+      return item.id === currentNode.id;
+    });
+
+    const nextSlide = {
+      [leftKeyCode]: {
+        currentStep: currentSlideIdx - 1,
+        slideStepEndPoint: state.length - 1,
+      },
+      [rightKeyCode]: {
+        currentStep: currentSlideIdx + 1,
+        slideStepEndPoint: 0,
+      },
+    }[e.keyCode];
+
+    if (!nextSlide) return;
+
+    const slide = state[nextSlide.currentStep];
+    const node = document.querySelector(`[id=${slide?.id}]`);
+
+    if (!node) {
+      currentNode.classList.remove("active");
+      slides[nextSlide.slideStepEndPoint].classList.add("active");
+    } else {
+      currentNode.classList.remove("active");
+      node.classList.add("active");
+    }
+  });
 
   slides.forEach((slide) => {
     slide.addEventListener("click", handleClick);
@@ -53,3 +87,14 @@ const setPhotos = () => {
 };
 
 fetchPhotos();
+
+// const isJopa // boolean
+// const user // object
+// const users // array
+// const getUser // function // вызываем, получаем после вызова
+// const getUsers // тоже самое что и выше
+// const onClick // вызываем
+// const handleRequest // вызываем
+// const convertData //
+// const convertedData //
+// const strJopa = 'jopa'
